ac1ce0c78c38e8bbe6a54eff68e82adc134c8a3b
aarrddi <ardiandro90@gmail.com>
2023-01-29 12:51:02 +0700

    hw belom jadi

--- homework.js
+++ homework.js
@@ -1,1 +1,8 @@
 console.log("ini PR")
+let arr = []
+
+for (let index = 0; index < 100; index++){
+        arr.push(Math.floor(Math.random()*100)+1)
+}
+console.log(arr)
+for (let index = 0; index <100 ; index++ )
